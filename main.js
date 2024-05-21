421e3b0ddaeb03015d2525285ee5e6742312a821
chriswebdv <chriswebdv@gmail.com>
2024-05-21 17:26:22 -0600

    Create nested loop to see how to add square to my HTML page using the DOM

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
-const square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
 
-console.log(square);
+for (let i = 0; i < 16; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+  console.log(square);
+  for (let j = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    console.log(square);
+  }
+}
